aa08e7b5d6b781e4a32a1bbc84081cfadfe7a805
Adriano Dias <adriano.rj.br@gmail.com>
2024-08-24 20:59:10 -0300

    add enter key on last column

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -10,3 +10,19 @@ TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   KC_X
 KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           KC_Y,              CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
 U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_Z,              KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
+
+#if defined (KEYBOARD_crkbd)
+  #define XXX KC_NO
+  #define MIRYOKU_MAPPING( \
+        K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+        K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+        K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+        N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+  ) \
+  LAYOUT_split_3x6_3( \
+  XXX,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  XXX, \
+  XXX,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  XXX, \
+  XXX,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  KC_ENT, \
+                    K32,  K33,  K34,         K35,  K36,  K37 \
+  )
+#endif
